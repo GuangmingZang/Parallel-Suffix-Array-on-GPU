e98f617de5c85fb402b0106d3b00718e56169d87
GangLiao <liaogang@liaogangs-MacBook-Pro.local>
2015-01-19 06:53:37 +0300

    fix some bugs

--- dc3_gpu/dc3.cu
+++ dc3_gpu/dc3.cu
@@ -6,7 +6,7 @@
 //  Copyright (c) 2014 gangliao. All rights reserved.
 //
 
-#include "head.h"
+#include "Head.h"
 
 
 
@@ -15,6 +15,7 @@ void read_data(char *filename, char *buffer, int num){
 	fh = fopen(filename, "r");
 	fread(buffer, 1, num, fh);
 	buffer[num] = '\0';
+
 	fclose(fh);
 }
 
@@ -45,25 +46,25 @@ int main(int argc, char* argv[])
 	printf("Please input the size of dataset you want to evaluate (10 - 1000000): \t");
 	scanf("%d", &n);
 
-	data = (char *) malloc((n+1)*sizeof(char));
+	data = (char *)malloc((n + 1)*sizeof(char));
 
 	read_data(filename, data, n);				//read data set from the local file
 
 
 	//inp = (int *)malloc( (n+3)*sizeof(int) );	//dynamic allocate memory
 	//SA  = (int *)malloc( (n+3)*sizeof(int) );
-	thrust::host_vector<int> h_inp(n+3);
-	thrust::host_vector<int> h_SA(n+3, 0);
+	thrust::host_vector<int> h_inp(n + 3);
+	thrust::host_vector<int> h_SA(n + 3, 0);
 	thrust::device_vector<int>d_inp;
 	thrust::device_vector<int>d_SA;
 
 
-	for(i=0;i<n;i++)							//Ascii 'A' -> integer 0 by 'A' - 65
+	for (i = 0; i<n; i++)							//Ascii 'A' -> integer 0 by 'A' - 65
 	{
 		h_inp[i] = to_i(data[i]);
 	}
 
-	h_inp[i]=0;h_inp[i+1]=0;h_inp[i+2]=0;				//prepare for triples
+	h_inp[i] = 0; h_inp[i + 1] = 0; h_inp[i + 2] = 0;				//prepare for triples
 	d_inp = h_inp;
 	d_SA = h_SA;
 
@@ -76,16 +77,16 @@ int main(int argc, char* argv[])
 	cudaEventSynchronize(stop);
 	cudaEventElapsedTime(&milliseconds, start, stop);
 
-	for(i = 0 ; i < n ; i++)					//print sorted suffixes from data set
+	/*for (i = 0; i < n; i++)					//print sorted suffixes from data set
 	{
 		printf("No.%d Index.", i);
 		print_suffix(data, h_SA[i]);
-	}
+	}*/
 
-	printf("GPU construct Suffix Array\nNUM: %d \t Time: %f Sec\n", n, milliseconds/1000);
+	printf("GPU construct Suffix Array\nNUM: %d \t Time: %f Sec\n", n, milliseconds / 1000);
 
-	cudaEventDestroy( start );
-	cudaEventDestroy( stop );
+	cudaEventDestroy(start);
+	cudaEventDestroy(stop);
 	free(data);									//free allocated memory
 	return 0;
 }
@@ -95,53 +96,60 @@ int main(int argc, char* argv[])
 
 void suffixArray(thrust::device_vector<int>& s, thrust::device_vector<int>& SA, int n, int K) {
 
-	int n0=(n+2)/3, n1= (n+1)/3, n2=n/3, n02=n0+n2;
+	int n0 = (n + 2) / 3, n1 = (n + 1) / 3, n2 = n / 3, n02 = n0 + n2;
 
-	thrust::device_vector<int>d_s12(n02+3, 0);
-	thrust::device_vector<int>d_SA12(n02+3, 0);
+	thrust::device_vector<int>d_s12(n02 + 3, 0);
+	int *pd_s12 = thrust::raw_pointer_cast(&d_s12[0]);
+
+	thrust::device_vector<int>d_SA12(n02 + 3, 0);
+	int *pd_SA12 = thrust::raw_pointer_cast(&d_SA12[0]);
+
 	thrust::device_vector<int>d_s0(n0, 0);
+	int *pd_s0 = thrust::raw_pointer_cast(&d_s0[0]);
+
 	thrust::device_vector<int>d_SA0(n0, 0);
-	thrust::device_vector<int>d_scan(n02+3);
+	int *pd_SA0 = thrust::raw_pointer_cast(&d_SA0[0]);
 
-	//std::cout << "d_s12 size: " << d_s12.size() << std::endl;
+	thrust::device_vector<int>d_scan(n02 + 3);
+	int *pd_scan = thrust::raw_pointer_cast(&d_scan[0]);
+	
+	
+	int *pd_s = thrust::raw_pointer_cast(&s[0]);
+	int *pd_SA = thrust::raw_pointer_cast(&SA[0]);
+
+	dim3 numThreads(1024, 1, 1);
+	dim3 numBlocks((n02 - 1) / 1024 + 1, 1, 1);
+
+	
 	// S12 initialization:
-	thrust::sequence(d_s12.begin(), d_s12.begin()+n02);
-	thrust::transform(d_s12.begin(), d_s12.begin()+n02, d_s12.begin(), mapping());
+	//thrust::sequence(d_s12.begin(), d_s12.begin() + n02);
+	//thrust::transform(d_s12.begin(), d_s12.begin() + n02, d_s12.begin(), mapping());
 
+	Init_d_s12 <<<numBlocks, numThreads >>>(pd_s12, n02);
 
-	dim3 numThreads(1024,1,1);
-	dim3 numBlocks((n02-1)/1024 + 1,1,1);
-
-	int *pd_s12 = thrust::raw_pointer_cast( &d_s12[0] );
-	int *pd_SA12 = thrust::raw_pointer_cast( &d_SA12[0] );
-	int *pd_s = thrust::raw_pointer_cast( &s[0] );
-	int *pd_s0 = thrust::raw_pointer_cast( &d_s0[0] );
-	int *pd_SA0 = thrust::raw_pointer_cast( &d_SA0[0] );
-	int *pd_SA = thrust::raw_pointer_cast( &SA[0] );
 	//radix sort - using SA12 to store keys
-	keybits<<<numBlocks, numThreads>>>(pd_SA12, pd_s12, pd_s , n02, 2);
+	keybits <<<numBlocks, numThreads >>>(pd_SA12, pd_s12, pd_s, n02, 2);
 	//cudaThreadSynchronize();
 
-	thrust::sort_by_key(d_SA12.begin(), d_SA12.begin()+n02, d_s12.begin());
+	thrust::sort_by_key(d_SA12.begin(), d_SA12.begin() + n02, d_s12.begin());
 
-	keybits<<<numBlocks, numThreads>>>(pd_SA12, pd_s12, pd_s , n02, 1);
-	thrust::sort_by_key(d_SA12.begin(), d_SA12.begin()+n02, d_s12.begin());
+	keybits <<<numBlocks, numThreads >>>(pd_SA12, pd_s12, pd_s, n02, 1);
+	thrust::sort_by_key(d_SA12.begin(), d_SA12.begin() + n02, d_s12.begin());
 
-	keybits<<<numBlocks, numThreads>>>(pd_SA12, pd_s12, pd_s , n02, 0);
-	thrust::sort_by_key(d_SA12.begin(), d_SA12.begin()+n02, d_s12.begin());
+	keybits <<<numBlocks, numThreads >>>(pd_SA12, pd_s12, pd_s, n02, 0);
+	thrust::sort_by_key(d_SA12.begin(), d_SA12.begin() + n02, d_s12.begin());
 
 	d_SA12 = d_s12;
 
-
 	// stably sort the mod 0 suffixes from SA12 by their first character
 	// find lexicographic names of triples
-	int *pd_scan = thrust::raw_pointer_cast( &d_scan[0] );
-	InitScan<<<numBlocks, numThreads>>>(pd_s, pd_SA12, pd_scan, n02);
+	
+	InitScan <<<numBlocks, numThreads>>>(pd_s, pd_SA12, pd_scan, n02);
 
-	thrust::exclusive_scan(d_scan.begin(), d_scan.end(), d_scan.begin());
+	thrust::exclusive_scan(d_scan.begin(), d_scan.begin() + n02 + 1, d_scan.begin());
 
 
-	Set_suffix_rank<<<numBlocks, numThreads>>>(pd_s12, pd_SA12, pd_scan, n02, n0);
+	Set_suffix_rank <<<numBlocks, numThreads >>>(pd_s12, pd_SA12, pd_scan, n02, n0);
 
 	//for(int i = 0; i < d_s12.size(); i++)
 	//							std::cout << "s12[" << i << "] = " << d_s12[i]<< std::endl;
@@ -154,46 +162,55 @@ void suffixArray(thrust::device_vector<int>& s, thrust::device_vector<int>& SA, 
 	// if max_rank is less than the size of s12, we have a repeat. repeat dc3.
 	// else generate the suffix array of s12 directly
 
-	if(max_rank < n02)
+	if (max_rank < n02)
 	{
-		suffixArray(d_s12,d_SA12,n02,max_rank);
-		Store_unique_ranks<<<numBlocks, numThreads>>>(pd_s12, pd_SA12, n02);
-	}else{
-		Compute_SA_From_UniqueRank<<<numBlocks, numThreads>>>(pd_s12, pd_SA12, n02);
+		suffixArray(d_s12, d_SA12, n02, max_rank);
+		Store_unique_ranks <<<numBlocks, numThreads >>>(pd_s12, pd_SA12, n02);
+	}
+	else{
+		Compute_SA_From_UniqueRank <<<numBlocks, numThreads >>>(pd_s12, pd_SA12, n02);
 	}
 
 
 
 
-	InitScan2<<<numBlocks, numThreads>>>(pd_SA12, pd_scan, n0, n02);
-	thrust::exclusive_scan(d_scan.begin(), d_scan.end(), d_scan.begin());
-	Set_S0<<<numBlocks, numThreads>>>(pd_s0, pd_SA12, pd_scan, n0, n02);
+	InitScan2 <<<numBlocks, numThreads >>>(pd_SA12, pd_scan, n0, n02);
+	thrust::exclusive_scan(d_scan.begin(), d_scan.begin() + n02, d_scan.begin()); //£¿£¿£¿£¿£¿£¿£¿£¿£¿£¿£¿£¿£¿£¿£¿£¿£¿
+	Set_S0 <<<numBlocks, numThreads >>>(pd_s0, pd_SA12, pd_scan, n0, n02);
 
 
-	dim3 numBlocks3((n0-1)/1024 + 1);
-	keybits<<<numBlocks3, numThreads>>>(pd_SA0, pd_s0, pd_s, n0, 0);
+	dim3 numBlocks3((n0 - 1) / 1024 + 1);
+	keybits <<<numBlocks3, numThreads >>>(pd_SA0, pd_s0, pd_s, n0, 0);
 	thrust::sort_by_key(d_SA0.begin(), d_SA0.begin() + n0, d_s0.begin());
 	d_SA0 = d_s0;
 
-	for(int i = 0; i < d_SA0.size(); i++)
-					std::cout << "SA0[" << i << "] = " << d_SA0[i]<< std::endl;
+	/*
+	for (int i = 0; i < d_SA0.size(); i++)
+		std::cout << "SA0[" << i << "] = " << d_SA0[i] << std::endl;
 
-	for(int i = 0; i < d_SA12.size(); i++)
-					std::cout << "SA12[" << i << "] = " << d_SA12[i]<< std::endl;
-
-
+	for (int i = 0; i < d_SA12.size(); i++)
+	{ 
+		if (d_SA12[i] < n0)
+			std::cout << "SA12[" << i << "] = " << d_SA12[i] * 3 + 1 << std::endl;
+		else
+			std::cout << "SA12[" << i << "] = " << (d_SA12[i]-n0) * 3 + 2 << std::endl;
+	}
+	*/
+	
 
 	// merge sorted SA0 suffixes and sorted SA12 suffixes
-	dim3 numBlocks2((n-1)/1024 + 1);
-	merge_suffixes<<<numBlocks, numThreads>>>(pd_SA0, pd_SA12, pd_SA, pd_s, pd_s12, n0, n02, n);
-	cudaDeviceSynchronize();
+	dim3 numBlocks2((n - 1) / 1024 + 1);
+	merge_suffixes <<<numBlocks, numThreads >>>(pd_SA0, pd_SA12, pd_SA, pd_s, pd_s12, n0, n02, n);
+	//cudaDeviceSynchronize();
 	///for(int i = 0; i < SA.size(); i++)
 	//	std::cout << "SA[" << i << "] = " << SA[i]<< std::endl;
 
-	for(int i = 0; i < s.size(); i++)
-		std::cout << "s[" << i << "] = " << (char)s[i]<< std::endl;
+	//for (int i = 0; i < s.size(); i++)
+		//std::cout << "s[" << i << "] = " << (char)s[i] << std::endl;
+	//d_SA0.clear();
+	//d_SA0.shrink_to_fit();
 
-
+	//d_SA12.clear();
+	//d_SA12.shrink_to_fit();
 
 }
-
--- dc3_gpu/head.h
+++ dc3_gpu/head.h
@@ -16,17 +16,19 @@
 #include <thrust/sort.h>
 #include <thrust/copy.h>
 #include <thrust/scan.h>
+#include <cuda.h>
+#include <cuda_runtime.h>
 #include <iostream>
 
-
-__global__ void keybits(int*, int*, int*, int, int);
-__global__ void  InitScan(int*  , int* , int*  , int);
-__global__ void Set_suffix_rank(int*  , int* , int*  , int, int);
-__global__ void  InitScan2(int*  , int* , int, int);
-__global__ void  Set_S0(int*  , int* , int*  , int, int);
+__global__ void		Init_d_s12(int*, int);
+__global__ void		keybits(int*, int*, int*, int, int);
+__global__ void		InitScan(int*, int*, int*, int);
+__global__ void		Set_suffix_rank(int*, int*, int*, int, int);
+__global__ void		InitScan2(int*, int*, int, int);
+__global__ void		Set_S0(int*, int*, int*, int, int);
 
 void print_arr(int*, int, int);
-void suffixArray(thrust::device_vector<int>& , thrust::device_vector<int>&, int , int ) ;
+void suffixArray(thrust::device_vector<int>&, thrust::device_vector<int>&, int, int);
 //void radixPass(int* , int* , int* , int , int );
 __global__ void merge_suffixes(int *, int *, int *, int *, int *, int, int, int, int);
 //int leq(int a1, int a2, int b1, int b2);
@@ -41,18 +43,18 @@ void print_suffix(char *cc, int i);
 #define GetI() (SA12[t] < n0 ? SA12[t] * 3 + 1 : (SA12[t] - n0) * 3 + 2)
 
 struct mapping {
-				__host__ __device__ int operator()(const int& x) const
-				{
-					return x + x/2 + 1;
-				}
-		};
+	__host__ __device__ int operator()(const int& x) const
+	{
+		return x + x / 2 + 1;
+	}
+};
 
 void print_suffix(char *cc, int i)
 {
 	int j = 0;
-	printf("%d: ",i);
-	for(j=i; j < strlen(cc);j++)
-		printf("%c",cc[j]);
+	printf("%d: ", i);
+	for (j = i; j < strlen(cc); j++)
+		printf("%c", cc[j]);
 	printf("\n");
 }
 
@@ -71,23 +73,37 @@ __device__ int leq(int a1, int a2, int b1, int b2) {
 __device__  int  leq2(int a1, int a2, int a3, int b1, int b2, int b3) {
 
 
-	return (a1 < b1 || (a1 == b1 && leq(a2,a3,b2,b3)));
+	return (a1 < b1 || (a1 == b1 && leq(a2, a3, b2, b3)));
 }
 
 
-void print_arr(int *arr, int no_arr,int dimension)
+void print_arr(int *arr, int no_arr, int dimension)
 {
 	int i = 0;
-	for(i=0; i<no_arr;i++){
+	for (i = 0; i<no_arr; i++){
 		printf("%d ", arr[i]);
-		if( (i+1)%dimension == 0)
+		if ((i + 1) % dimension == 0)
 			printf(", ");
 	}
 	printf("\n");
 }
 
 
+__global__ void Init_d_s12(int* s12, int n)
 
+{
+
+	int index = blockIdx.x*blockDim.x + threadIdx.x;
+
+	if (index >= n)
+
+		return;
+
+	s12[index] = index + index / 2 + 1;
+
+	//printf("%d %d\n", index, s12[index]);
+
+}
 
 
 //d_SA12, d_s12, n02
@@ -95,7 +111,7 @@ __global__ void keybits(int* SA12, int* s12, int* s, int n, int i)
 {
 	int index = blockIdx.x*blockDim.x + threadIdx.x;
 
-	if(index >= n)
+	if (index >= n)
 		return;
 	SA12[index] = s[s12[index] + i];
 
@@ -105,9 +121,9 @@ __global__ void keybits(int* SA12, int* s12, int* s, int n, int i)
 __global__ void  InitScan(int* s, int* SA12, int* scan, int n)
 {
 	int index = blockIdx.x*blockDim.x + threadIdx.x;
-	if(index >= n)
-			return;
-	if((s[SA12[index]] == s[SA12[index + 1]]) && (s[SA12[index] + 1] == s[SA12[index + 1] + 1]) && (s[SA12[index] + 2] == s[SA12[index + 1] + 2]))
+	if (index >= n)
+		return;
+	if ((s[SA12[index]] == s[SA12[index + 1]]) && (s[SA12[index] + 1] == s[SA12[index + 1] + 1]) && (s[SA12[index] + 2] == s[SA12[index + 1] + 2]))
 	{
 		scan[index] = 0;
 	}
@@ -120,8 +136,8 @@ __global__ void  InitScan(int* s, int* SA12, int* scan, int n)
 __global__ void Set_suffix_rank(int*  s12, int* SA12, int*  scan, int n02, int n0)
 {
 	int index = blockIdx.x*blockDim.x + threadIdx.x;
-		if(index >= n02)
-				return;
+	if (index >= n02)
+		return;
 
 	s12[SA12[index] / 3 + ((SA12[index] % 3) - 1) * n0] = scan[index] + 1;
 }
@@ -130,8 +146,8 @@ __global__ void Set_suffix_rank(int*  s12, int* SA12, int*  scan, int n02, int n
 __global__ void Store_unique_ranks(int*  s12, int*  SA12, int n)
 {
 	int index = blockIdx.x*blockDim.x + threadIdx.x;
-			if(index >= n)
-					return;
+	if (index >= n)
+		return;
 
 	s12[SA12[index]] = index + 1;
 
@@ -141,31 +157,31 @@ __global__ void Store_unique_ranks(int*  s12, int*  SA12, int n)
 __global__ void Compute_SA_From_UniqueRank(int*  s12, int* SA12, int n)
 {
 	int index = blockIdx.x*blockDim.x + threadIdx.x;
-				if(index >= n)
-						return;
+	if (index >= n)
+		return;
 	SA12[s12[index] - 1] = index;
 }
 
 
 
 __global__ void  InitScan2(int* SA12, int* scan, int n0, int n02)
-	{
-		int index = blockIdx.x*blockDim.x + threadIdx.x;
-		if(index >= n02)
-			return;
-		if(SA12[index] < n0)
-			scan[index] = 1;
-		else
-			scan[index] = 0;
-	}
+{
+	int index = blockIdx.x*blockDim.x + threadIdx.x;
+	if (index >= n02)
+		return;
+	if (SA12[index] < n0)
+		scan[index] = 1;
+	else
+		scan[index] = 0;
+}
 
 __global__ void  Set_S0(int* s0, int* SA12, int* scan, int n0, int n02)
 {
 	int index = blockIdx.x*blockDim.x + threadIdx.x;
-	if(index >= n02)
+	if (index >= n02)
 		return;
-	if(SA12[index] < n0)
-		s0[scan[index]] = 3*SA12[index];
+	if (SA12[index] < n0)
+		s0[scan[index]] = 3 * SA12[index];
 
 }
 
@@ -175,83 +191,93 @@ __global__ void merge_suffixes(int* SA0, int* SA12, int* SA, int* s, int* s12, i
 {
 	int index = blockIdx.x*blockDim.x + threadIdx.x;
 	int left, right, mid;
-	if(index >= n0 + n02)
+	int flag = 0;
+	if (index >= n0 + n02)
 		return;
 
-    int i, j;
-    if(index < n0)
-    {
-    	i = SA0[index];
-    	left = n0;
-    	right = n0 + n02;
+	if (n != n0 + n02)
+	{
+		flag = 1;
+		if (index == n0)
+			return;	
+	}
+	//if (n == n0 + n02 && index == n0 + n02 - 1)
+		//return;
 
-    	while(left < right)
-    	{
-    		mid = (left + right) / 2;
-    		if(SA12[mid - n0] < n0)
-    		{
-    			j = SA12[mid - n0] * 3 + 1;
+	int i, j;
+	if (index < n0)
+	{
+		i = SA0[index];
+		left = n0;
+		right = n0 + n02;
 
-    			if(leq(s[j], s12[(j+1)/3+((j+1)%3 - 1)*n0], s[i], s12[i/3]))
-    			    left = mid + 1;
-    			else
-    			    right = mid;
-    		}
-    		else
-    		{
-    			j = (SA12[mid - n0] - n0) * 3 + 2;
+		while (left < right)
+		{
+			mid = (left + right) / 2;
+			if (SA12[mid - n0] < n0)
+			{
+				j = SA12[mid - n0] * 3 + 1;
 
-    			if(leq2(s[j], s[j+1], s12[(j+2)/3+((j+2)%3 - 1)*n0],s[i], s[i+1], s12[i/3+n0]))
-    			    left = mid + 1;
-    			else
-    			    right = mid;
-    		}
+				if (leq(s[j], s12[(j + 1) / 3 + ((j + 1) % 3 - 1)*n0], s[i], s12[i / 3]))
+					left = mid + 1;
+				else
+					right = mid;
+			}
+			else
+			{
+				j = (SA12[mid - n0] - n0) * 3 + 2;
 
-    	}
-    	//if(i == 0)
-    		printf("%%%%SA[%d] = %d index %d\n", index-n0+left, i, index);
-    	SA[index + left - n0] = i;
+				if (leq2(s[j], s[j + 1], s12[(j + 2) / 3 + ((j + 2) % 3 - 1)*n0], s[i], s[i + 1], s12[i / 3 + n0]))
+					left = mid + 1;
+				else
+					right = mid;
+			}
 
-    }
-    else
-    {
-    	if(SA12[index - n0] < n0 )
-    	{
-    		i = SA12[index - n0] * 3 + 1;
-    		left = 0;
-    		right = n0;
-    		//leq(s[i], s12[SA12[t] + n0], s[j], s12[j/3])
-    		while(left < right)
-    		{
-    			mid = (left + right)/2;
-    			//a1 < b1 || (a1 == b1 && a2 <= b2)
+		}
+		//if(i == 0)
+	//	printf("%%%%SA[%d] = %d index %d\n", index - n0 + left, i, index);
+		SA[index + left - n0 - flag] = i;
 
-    			if(leq(s[SA0[mid]], s12[SA0[mid]/3], s[i], s12[(i+1)/3+((i+1)%3 - 1)*n0]))
-    				left = mid + 1;
-    			else
-    				right = mid;
-    		}
-    		SA[index - n0 + left] = i;
-    		//if(i == 0)
-    	   		printf("@@@SA[%d] = %d (%c %d) index: %d\n", index-n0+left, i, s[i], s12[(i+1)/3+((i+1)%3 - 1)*n0], index);
-    	}
-    	else
-    	{
-    		i = (SA12[index - n0] - n0) * 3 + 2;
-    		left = 0;
-    		right = n0;
-    		while(left < right)
-    		{
-    			mid = (left + right)/2;
+	}
+	else
+	{
+		if (SA12[index - n0] < n0)
+		{
+			i = SA12[index - n0] * 3 + 1;
+			left = 0;
+			right = n0;
+			//leq(s[i], s12[SA12[t] + n0], s[j], s12[j/3])
+			while (left < right)
+			{
+				mid = (left + right) / 2;
+				//a1 < b1 || (a1 == b1 && a2 <= b2)
 
-    			if(leq2(s[SA0[mid]], s[SA0[mid]+1], s12[SA0[mid]/3+n0],s[i], s[i+1], s12[(i+2)/3+((i+2)%3 - 1)*n0]))
-    				left = mid + 1;
-    			else
-    				right = mid;
-    		}
-    		SA[index - n0 + left] = i;
+				if (leq(s[SA0[mid]], s12[SA0[mid] / 3], s[i], s12[(i + 1) / 3 + ((i + 1) % 3 - 1)*n0]))
+					left = mid + 1;
+				else
+					right = mid;
+			}
+			SA[index - n0 + left - flag] = i;
+			//if(i == 0)
+			//printf("@@@SA[%d] = %d (%c %d) index: %d\n", index - n0 + left, i, s[i], s12[(i + 1) / 3 + ((i + 1) % 3 - 1)*n0], index);
+		}
+		else
+		{
+			i = (SA12[index - n0] - n0) * 3 + 2;
+			left = 0;
+			right = n0;
+			while (left < right)
+			{
+				mid = (left + right) / 2;
 
-    		printf("xxxxSA[%d] = %d (%c %c %d) index %d \n", index-n0+left, i, s[i], s[i+1], s12[(i+2)/3+((i+2)%3 - 1)*n0], index);
-    	}
-    }
+				if (leq2(s[SA0[mid]], s[SA0[mid] + 1], s12[SA0[mid] / 3 + n0], s[i], s[i + 1], s12[(i + 2) / 3 + ((i + 2) % 3 - 1)*n0]))
+					left = mid + 1;
+				else
+					right = mid;
+			}
+			SA[index - n0 + left - flag] = i;
+
+			//printf("xxxxSA[%d] = %d (%c %c %d) index %d \n", index - n0 + left, i, s[i], s[i + 1], s12[(i + 2) / 3 + ((i + 2) % 3 - 1)*n0], index);
+		}
+	}
 }
